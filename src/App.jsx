1232354ec2b5dde3fa1b55c61b792d1f415c91c5
Mohammed Subhan <mohdsubhan2764@gmail.com>
2023-07-19 22:55:16 +0530

    First commit - create a header and connected with ract router dom

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './styles/App.css';
+import { BrowserRouter as Router, Route,Routes} from 'react-router-dom'
+import Header from './components/Header';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import Product from './components/Product';
+import About from './components/About';
+function App() {
+  return (
+    <>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route  path="/" element={<Home/>}/>
+        <Route  path="/about" element={<About/>}/>
+        <Route  path="/contact" element={<Contact/>}/>
+        <Route  path="/product" element={<Product/>}/>
+      </Routes>
+    </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/header.css'
+const Header = () => {
+  return (
+    <div className="header">
+        <nav>
+            <ul>
+                <li><Link to="/" className='link'>Home</Link></li>
+                <li><Link to="/about" className='link'>About</Link></li>
+                <li><Link to="/contact" className='link'>Contact</Link></li>
+                <li><Link to="/product" className='link'>Product</Link></li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Header
